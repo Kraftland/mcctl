21cbc20a2e3ba6d2bc347e3dea21b3af054f1466
BuildTools <unconfigured@null.spigotmc.org>
2022-06-29 15:59:30 +0800

    Fixed output messages

--- Update.sh
+++ Update.sh
@@ -16,12 +16,13 @@ function exitScript(){
     elif [ $@ = 2 ]; then
         sign='Can not create directory'
     elif [ $@ =3 ]; then
-        code3='Non-64-bit system detected'
+        sign='Non-64-bit system detected'
     else
-        echo "Undefined error code"
+        sign="Undefined error code"
     fi
-    echo "${sign}"
-    echo 'Exitting...'
+    echo "[Critical] ${sign}"
+    echo '[Critical] Exitting...'
+    unset ${sign}
     exit $@
 }
 
@@ -34,7 +35,7 @@ function createFolder(){
         if [ $? = 1 ]; then
         echo '[Info] mkdir returned error code 1, retrying with sudo'
             if [ $@ =~ 'unattended' ]; then
-                echo '[Warn] unattended flag detected, terminating...'
+                echo '[Warn] unattended flag detected.'
                 exitScript 2
             else
                 if [ `whoami` = root ]; then
@@ -83,9 +84,9 @@ function buildMojang(){
 function buildSpigot(){
     url="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
     checkFile=BuildTools.jar
-    echo "Downloading BuildTools for Spigot..."
+    echo "[Info] Downloading BuildTools for Spigot..."
     wget ${url} >/dev/null
-    java -jar $checkFile nogui --rev ${version} >/dev/null
+    java -jar $checkFile nogui --rev ${version} >/dev/null 2>/dev/null
     rm -rf ${checkConfig}
     mv spigot-*.jar Spigot-latest.jar
     update Spigot-latest.jar
@@ -93,15 +94,15 @@ function buildSpigot(){
 
 #testPackageManager
 function detectPackageManager(){
-    echo "Detecting package manager..."
+    echo "[Info] Detecting package manager..."
     if [[ $(sudo apt install ) ]]; then
-        echo 'Detected apt'
+        echo '[Info] Detected apt'
         return apt
     elif [[ $(sudo pacman -h ) ]]; then
-        echo 'Detected pacman'
+        echo '[Info] Detected pacman'
         return pacman
     elif [[ $(sudo dnf install ) ]]; then
-        echo 'Detected dnf'
+        echo '[Info] Detected dnf'
         return dnf
     else
         return unknown
@@ -110,12 +111,10 @@ function detectPackageManager(){
 #checkConfig
 checkConfig(){
     if [ ! ${version} ]; then
-        echo '$version not set, please enter your desied version:'
-        read version
+        exitScript 4
     fi
     if [ ! ${serverPath} ]; then
-        echo "Warning! serverPath not set, please enter complete path to your server:"
-        read serverPath
+        exitScript 4
     fi
     if [ ! $build ]; then
         export build=500
@@ -123,7 +122,7 @@ checkConfig(){
 }
 #removeJarFile
 function clean(){
-    echo "Cleaning..."
+    echo "[Info] Cleaning."
     rm -rf *.jar
     rm -rf *.check
     rm -rf *.1
@@ -131,7 +130,7 @@ function clean(){
 }
 #moveFile
 function update(){
-    echo "Updating jar file..."
+    echo "[Info] Updating jar file..."
     if [[ $@ = "Paper-latest.jar" ]]; then
         mv $@ ${serverPath}
     elif [[ $@ = "Spigot-latest.jar" ]]; then
@@ -142,7 +141,7 @@ function update(){
 }
 #versionCompare
 function versionCompare(){
-    echo "Making sure you're up to date..."
+    echo "[Info] Making sure you're up to date."
     if [ $isPlugin = true ]; then
         checkPath="${serverPath}/plugins"
     else
@@ -153,12 +152,12 @@ function versionCompare(){
 }
 #integrityProtect
 function integrityProtect(){
-    echo "Checking file integrity..."
+    echo "[Info] Checking file integrity."
     if [[ $@ =~ "unsafe" ]]; then
-        echo "Warning! Default protection disabled. USE AT YOUR OWN RISK!"
+        echo "[Warn] Default protection disabled. USE AT YOUR OWN RISK!"
         return 0
     else
-        echo "Verifing ${checkFile}"
+        echo "[Info] Verifing ${checkFile}"
         if [ ${isPlugin} = false ]; then
             checkFile=Paper-latest.jar
             wget $url >/dev/null
@@ -173,10 +172,10 @@ function integrityProtect(){
         fi
     fi
     if [ $? = 1 ]; then
-        echo "Checking job done, repairing ${checkFile}."
+        echo "[Warn] Checking job done, repairing ${checkFile}."
         redownload
     else
-        echo "Ckecking job done, ${ckeckFile} verified."
+        echo "[Info] Ckecking job done, ${ckeckFile} verified."
         clean
     fi
 }
@@ -194,23 +193,23 @@ function redownload(){
 }
 #pluginUpdate
 function pluginUpdate(){
-    echo "Updating ${checkFile}"
+    echo "[Info] Updating ${checkFile}"
     if [ $@ = Floodgate ]; then
-        pluginName=Floodgate
+        pluginName="$@"
         url="https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar"
     elif [ $@ = Geyser ]; then
-        pluginName=Geyser
+        pluginName="$@"
         url="https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar"
     elif [ $@ = SAC ]; then
-        pluginName=SAC
+        pluginName="$@"
         url="https://www.spigotmc.org/resources/soaromasac-lightweight-cheat-detection-system.87702/download?version=455200"
     elif [ $@ = MTVehicles ]; then
         pluginName="$@"
         url="https://www.spigotmc.org/resources/mtvehicles-vehicle-plugin-free-downloadable.80910/download?version=452759"
     else
-        echo "Sorry, but we don't have your plugin's download url. Please wait for support~"
+        echo "[Warn] Sorry, but we don't have your plugin's download url. Please wait for support~"
     fi
-    echo "Downloading ${pluginName}"
+    echo "[Info] Downloading ${pluginName}"
     wget $url >/dev/null
     isPlugin=true
 }
@@ -218,40 +217,40 @@ function pluginUpdate(){
 function systemUpdate(){
     if [[ $@ =~ 'nosudo' ]]; then
         if [ $? = apt ]; then
-            echo "Updating using apt..."
+            echo "[Info] Updating using apt..."
             apt -y full-upgrade
         elif [ $? = dnf ]; then
-            echo "Updating using dnf..."
+            echo "[Info] Updating using dnf..."
             dnf -y update
         elif [ $? = pacman ]; then
-            echo "Updating using pacman..."
+            echo "[Info] Updating using pacman..."
             pacman --noconfirm -Syyu
         else
             unset packageManager
-            echo "Package Manager not found! Enter command to update or type 'skip' to skip"
+            echo "[Warn] Package manager not found! Enter command to update or type 'skip' to skip"
             read packageManager
             if [ ! ${packageManager} = skip ]; then
                 ${packageManager}
             else
-                echo "Skipping"
+                echo "[Info] Skipping"
             fi
         fi
     else
         if [ $? = apt ]; then
-            echo "Updating using apt..."
+            echo "[Info] Updating using apt..."
             sudo apt -y full-upgrade
         elif [ $? = dnf ]; then
-            echo "Updating using dnf..."
+            echo "[Info] Updating using dnf..."
             sudo dnf -y update
         elif [ $? = pacman ]; then
-            echo "Updating using pacman..."
+            echo "[Info] Updating using pacman..."
             sudo pacman --noconfirm -Syyu
         else
             unset packageManager
             if [[ $@ =~ "unattended" ]];then
-                echo "unattended flag detected, skipping system update due to unknown package manager..."
+                echo "[Warn] unattended flag detected, skipping system update due to unknown package manager..."
             else
-                echo "Package Manager not found! Enter command to update or type 'skip' to skip"
+                echo "[Info] Package Manager not found! Enter command to update or type 'skip' to skip"
                 read packageManager
                 if [ ! ${packageManager} = skip ]; then
                     if [[ ${packageManager} =~ 'sudo' ]]; then
@@ -260,7 +259,7 @@ function systemUpdate(){
                         ${packageManager}
                     fi
                 else
-                    echo "Skipping"
+                    echo "[Info] Skipping"
                 fi
             fi
         fi
@@ -271,11 +270,11 @@ function systemUpdate(){
 function buildPaper(){
     while [ ! -f paper-*.jar ]; do
         export build=`expr ${build} - 1`
-        echo "Testing build ${build}"
+        echo "[Info] Testing build ${build}"
         url="https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/paper-${version}-${build}.jar"
         wget $url >/dev/null
     done
-    echo "Downloaded build ${build}."
+    echo "[Info] Downloaded build ${build}."
     if [ -f paper-*.jar ]; then
         mv paper-*.jar Paper-latest.jar
     fi
@@ -284,10 +283,10 @@ function buildPaper(){
     integrityProtect
     versionCompare
     if [ $? = 0 ]; then
-        echo "You're up to date."
+        echo "[Info] You're up to date."
         clean
     else
-        echo "Updating Paper..."
+        echo "[Info] Updating Paper..."
         update Paper-latest.jar
     fi
     clean
@@ -298,21 +297,20 @@ function checkBit(){
     getconf LONG_BIT
     return $?
     if [ $? = 64 ]; then
-        echo "Running on 64-bit system."
+        echo "[Info] Running on 64-bit system."
     elif [ $? = 32 ]; then
         if [[ $@ =~ "unsafe" ]]; then
-            echo "Warning at `date`, running on 32-bit system may encounter unexpected problems."
+            echo "[Warn] Warning at `date`, running on 32-bit system may encounter unexpected problems."
         else
-            echo "32-bit system detected, script is stopping..."
             exitScript 3
         fi
     fi
 }
 
 function main(){
-    echo "Hello! `whoami` at `date`"
+    echo "[Info] Hello! `whoami` at `date`"
     checkBit
-    echo "Reading settings"
+    echo "[Info] Reading settings"
     clean
     checkConfig
     if [[ $@ =~ 'newserver' ]]; then
@@ -320,7 +318,7 @@ function main(){
     fi
 
     ######Paper Update Start######
-    echo "Starting auto update at `date`"
+    echo "[Info] Starting auto update at `date`"
     cd ${serverPath}/Update/
     if [[ $@ =~ "paper" ]]; then
         buildPaper
@@ -364,7 +362,7 @@ function main(){
     fi
 
     if [[ $@ =~ "sac" ]]; then
-        echo "Warning! Beta support for SoaromaSAC"
+        echo "[Warn] Warning! Beta support for SoaromaSAC"
         isPlugin=true
         unset checkFile
         update *.jar
@@ -377,7 +375,7 @@ function main(){
     fi
     rm -rf ${serverPath}/plugins/BuildTools.jar
     clean
-    echo "Job finished at `date`, have a nice day~"
+    echo "[Info] Job finished at `date`, have a nice day~"
     exitScript 0
 }
 
